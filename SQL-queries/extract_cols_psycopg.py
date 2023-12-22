9fe449b823c7871096a948545acbd1b52aca2883
selvatica-36 <silvia.h.aragon@gmail.com>
2023-12-22 16:16:24 +0000

    Loops through all tables and extracts column names

--- SQL-queries/extract_cols_psycopg.py
+++ SQL-queries/extract_cols_psycopg.py
@@ -5,7 +5,7 @@ import os
 
 
 # Read database connection parameters from config.json
-with open('settings.json', 'r') as settings_file:
+with open('settings-copy.json', 'r') as settings_file:
     connection_params = json.load(settings_file)
 
 # Connect to the database
@@ -13,7 +13,7 @@ conn = psycopg2.connect(**connection_params)
 cursor = conn.cursor()
 
 # Get the path to the folder where you want to save CSV files
-output_folder = '/Users/silviaaragon/Aicore/Online-Shopping-in-Retail-Report/SQL-queries'
+output_folder = '/Users/silviaaragon/Aicore/Online-Shopping-in-Retail-Report/SQL-queries/data-info'
 
 # Get a list of tables in the database
 cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';")
@@ -21,12 +21,15 @@ tables = [row[0] for row in cursor.fetchall()]
 
 # Loop through each table and export its columns to a CSV file
 for table in tables:
-    cursor.execute(f"COPY (SELECT column_name, data_type FROM information_schema.columns WHERE table_name = '{table}') TO STDOUT WITH CSV HEADER;")
-    csv_path = os.path.join(output_folder, f"{table}-columns.csv")
+    csv_path = os.path.join(output_folder, f"{table}_columns.csv")
     with open(csv_path, "w", newline="") as csv_file:
         csv_writer = csv.writer(csv_file)
-        csv_writer.writerow([desc[0] for desc in cursor.description])  # Write column headers
-        csv_writer.writerows(cursor)
+        # Get column names and data types
+        cursor.execute(f"SELECT column_name, data_type FROM information_schema.columns WHERE table_name = '{table}'")
+        columns = cursor.fetchall()
+        csv_writer.writerow([column[0] for column in columns])  # Write column headers
+        # Use the COPY (SELECT ...) TO variant to export data to the CSV file
+        cursor.copy_expert(f"COPY (SELECT * FROM {table}) TO STDOUT WITH CSV HEADER;", csv_file)
 
 
 # Close the database connection
